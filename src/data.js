9c625645b2d8651f15d1baad8928cdc1381bc9f8
abhiaswale <aswaleabhishek8@gmail.com>
2022-03-25 15:42:24 +0530

    added new project

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export const projectItems = [
   {
     id: 1,
-    name: "TO DO APP",
-    date: "8th Oct 2021",
-    description: "An basic To Do tasks App to add tasks ",
+    name: "BLOGGER",
+    date: "25th Mar 2022",
+    description: "A Blog Application to post blogs",
     detailedDescription:
-      "Created this ToDo App using React, styled using css modules. User can add any tasks needed to be done and the tasks can be deleted once completed. used firebase as backend for storing data",
-    live: "https://todoapp-e2fc65.netlify.app/",
-    source: "https://github.com/abhiaswale/todolist",
+      "Created this app in MERN stack,useContext for state Management, Tailwind for CSS, Cloudinary for image upload. Core features include Create Blog with text and image, Posting shared content, Blogs Feed, Search Blogs,Update existing blogs, JWT based Authentication",
+    live: "https://blog-app03.netlify.app/",
+    source: "https://github.com/abhiaswale/blogApp-frontend",
   },
   {
     id: 2,
-    name: "FOODY APP",
-    date: "1st Nov 2021",
-    description: "An App made for an Restaurant",
+    name: "CRYPTO APP",
+    date: "25th Jan 2022",
+    description: "An App to check prices of cryptocurrencies",
     detailedDescription:
-      "Created this app using react, used Redux toolkit and styling was done using css modules. Core Functionality includes Login, Signup, Booking a table. used firebase as a backend for storing data ",
-    live: "https://foody-app-12167a.netlify.app/",
-    source: "https://github.com/abhiaswale/thefoodyapp",
+      "An App to check prices of various crypto currencies. Made using React and Tailwind Css and used axios to fetch data from Api. User can search for diffrent cryptos view their details like current price %up, %down, etc",
+    live: "https://cryptocurrencyapp1.netlify.app/",
+    source: "https://github.com/abhiaswale/crypto-app",
   },
   {
     id: 3,
@@ -31,13 +31,13 @@ export const projectItems = [
   },
   {
     id: 4,
-    name: "CRYPTO APP",
-    date: "25th Jan 2022",
-    description: "An App to check prices of cryptocurrencies",
+    name: "TO DO APP",
+    date: "8th Oct 2021",
+    description: "An basic To Do tasks App to add tasks ",
     detailedDescription:
-      "An App to check prices of various crypto currencies. Made using React and Tailwind Css and used axios to fetch data from Api. User can search for diffrent cryptos view their details like current price %up, %down, etc",
-    live: "https://cryptocurrencyapp1.netlify.app/",
-    source: "https://github.com/abhiaswale/crypto-app",
+      "Created this ToDo App using React, styled using css modules. User can add any tasks needed to be done and the tasks can be deleted once completed. used firebase as backend for storing data",
+    live: "https://todoapp-e2fc65.netlify.app/",
+    source: "https://github.com/abhiaswale/todolist",
   },
   {
     id: 5,
@@ -48,5 +48,15 @@ export const projectItems = [
       "An App to check weather conditions of diffrent cities around the world. Made using React and Tailwind Css and used axios to fetch data from Open Weather Map Api. Weather conditions like Current temperature, Humidity, Min Temp, Max Temp, Wind Speed, etc can be viewed ",
     live: "https://weather-app-dd52.netlify.app/",
     source: "https://github.com/abhiaswale/weather-app",
+  },
+  {
+    id: 6,
+    name: "FOODY APP",
+    date: "1st Nov 2021",
+    description: "An App made for an Restaurant",
+    detailedDescription:
+      "Created this app using react, used Redux toolkit and styling was done using css modules. Core Functionality includes Login, Signup, Booking a table. used firebase as a backend for storing data ",
+    live: "https://foody-app-12167a.netlify.app/",
+    source: "https://github.com/abhiaswale/thefoodyapp",
   },
 ];
